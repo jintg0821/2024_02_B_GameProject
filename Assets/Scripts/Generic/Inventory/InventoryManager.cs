119a470d5f08c060e235ff42b218accc548c1cca
jintg0821 <jintg0825@gmail.com>
2024-11-14 11:02:08 +0900

    [Feat] 조합 시스템 세팅

--- Assets/Scripts/Generic/Inventory/InventoryManager.cs
+++ Assets/Scripts/Generic/Inventory/InventoryManager.cs
@@ -1,19 +1,39 @@
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-//모든 아이템의 기본 인터페이스 interface 클래스
-//메소드, 이벤트, 인덱서, 프로퍼티
-//모든이 무조건 Public으로 선언 된다.
-//구현부가 없다.
+//모든 아이템의 기본 인터페이스 interface 클래스 
+//메소드,이벤트,인데서,프로퍼티 
+//모든이 무조건 Public으로 선언 된다. 
+//구현부가 없다. 
 
 public interface IItem
 {
     string Name { get; }
     int ID { get; }
-
     void Use();
 }
 
+//CraftingMaterial 클래스 추가 
+public class CraftingMaterial : IItem
+{
+    public string Name { get; private set; }
+    public int ID { get; private set; }
+
+    public CraftingMaterial(string name, int id)
+    {
+        Name = name;
+        ID = id;
+    }
+
+    public void Use()
+    {
+        Debug.Log($"This is a crafting material : {Name}");
+    }
+}
+
+//구체적인 아이템 클래스 (Weapon)
 public class Weapon : IItem
 {
     public string Name { get; private set; }
@@ -31,6 +51,7 @@ public class Weapon : IItem
         Debug.Log($"Using weapon {Name} with damage {Damage}");
     }
 }
+
 //구체적인 아이템 클래스 (HealthPotion)
 public class HealthPotion : IItem
 {
@@ -38,7 +59,7 @@ public class HealthPotion : IItem
     public int ID { get; private set; }
     public int HealAmount { get; private set; }
 
-    public HealthPotion(string name, int id, int healAmount)    //생성자
+    public HealthPotion(string name, int id, int healAmount)  //생성자
     {
         Name = name;
         ID = id;
@@ -50,7 +71,7 @@ public class HealthPotion : IItem
     }
 }
 
-//제네릭 인벤토리 클래스
+//제네릭 인벤토리 클래스 
 public class Inventory<T> where T : IItem
 {
     private List<T> items = new List<T>();
@@ -69,48 +90,90 @@ public class Inventory<T> where T : IItem
         }
         else
         {
-            Debug.Log("invalid item index");
+            Debug.Log("Invalid item index");
         }
     }
     public void ListItems()
     {
         foreach (var item in items)
         {
-            Debug.Log($"item: {item.Name} , ID : {item.ID}");
+            Debug.Log($"Item: {item.Name} , ID : {item.ID}");
         }
     }
+
+    public void RemoveItems(int itemId, int amount)
+    {
+        int removed = 0;
+        for (int i = items.Count - 1; i >= 0; i--)
+        {
+            if (items[i].ID == itemId)
+            {
+                items.RemoveAt(i);
+                removed++;
+                if (removed >= amount)
+                    break;
+            }
+        }
+    }
+
+    public bool HasEnough(int itemId, int amount)                   //아이템이 충분한지 검사 
+    {
+        return GetItemCount(itemId) >= amount;
+    }
+
+    public int GetItemCount(int itemId)                             //아이템 카운트 함수 
+    {
+        return items.Count(item => item.ID == itemId);
+
+    }
+
 }
 
-//인벤토리 Manager
+//인벤토리 Manager 
 public class InventoryManager : MonoBehaviour
 {
-    private Inventory<IItem> playerInventory;
+    private Inventory<IItem> playerInventory = new Inventory<IItem>();
     public int UseBagIndex;
 
     void Start()
     {
         playerInventory = new Inventory<IItem>();
 
-        //아이템 추가
+        //아이템 추가 
         playerInventory.AddItem(new Weapon("Sword", 1, 10));
         playerInventory.AddItem(new HealthPotion("Small Potion", 2, 20));
+
+        playerInventory.AddItem(new CraftingMaterial("Iron Ingot", 101));           //ID 101 : 철 주괴
+        playerInventory.AddItem(new CraftingMaterial("Iron Ingot", 101));           //ID 101 : 철 주괴
+        playerInventory.AddItem(new CraftingMaterial("Wood", 102));                 //ID 102 : 나무
+
+        playerInventory.AddItem(new CraftingMaterial("Herb", 102));                 //ID 201 : 약초
+        playerInventory.AddItem(new CraftingMaterial("Herb", 102));                 //ID 201 : 약초
+        playerInventory.AddItem(new CraftingMaterial("Water", 202));                 //ID 202 : 물
+
+    }
+
+    //인벤토리 접근자 메서드 추가 
+    public Inventory<IItem> GetInventory()
+    {
+        return playerInventory;
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            playerInventory.ListItems();        //인벤토리 내용 출력
+            playerInventory.ListItems();            //인벤토리 내용 출력
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            playerInventory.UseItem(UseBagIndex);         //첫번째 아이템 사용
+            playerInventory.UseItem(UseBagIndex);             //아이템 사용
         }
-        
+
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            playerInventory.AddItem(new Weapon("Sword", 1, 10));        //아이템 생성
+            playerInventory.AddItem(new Weapon("Sword", 1, 10));        //아이템 생성 
         }
     }
 }
